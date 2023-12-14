e569d4f9bf452923d215c9a79e7061296e20b5c6
EricL <ericlaudrum@gmail.com>
2023-12-14 15:36:30 -0500

    Clean up how results are output Add a visual tracker for each round played

--- script.js
+++ script.js
@@ -1,25 +1,48 @@
-let limit = 3;
+let roundNumber = 1;
+let limit = 5;
 let playerScore = 0;
 let cpuScore = 0;
 
+let winMessage = 'Yon won that one';
+let loseMessage = 'You lost that round';
+let tieMessage = 'It\'s a tie!';
+let champMessage = 'CONGRADULATIONS'
+let consolationMessage = 'JUST TRY AGAIN. I WON\'T TELL IF YOU DON\'T'
+let errorMessage = 'Error'
+
+
+
+
+let results = ''
+
+let roundNumberElement = document.getElementsByClassName('round');
+roundNumberElement[0].innerText = roundNumber;
+console.log('round number', roundNumberElement)
+
+let limitElement = document.getElementsByClassName('limit');
+limitElement[0].innerText = limit;
+limitElement[1].innerText = limit;
+console.log('limit number', limitElement)
+
 let playerScoreElement = document.getElementsByClassName('playerScore');
+playerScoreElement[0].innerText = playerScore;
+
 let cpuScoreElement = document.getElementsByClassName('cpuScore');
-let resultsElement = document.getElementsByClassName('results')
+cpuScoreElement[0].innerText = cpuScore;
+
+let playerHandElement = document.getElementsByClassName('playerHand');
+let cpuHandElement = document.getElementsByClassName('cpuHand');
+
+let resultsElement = document.getElementsByClassName('results');
+resultsElement[0].innerText = results
+
+let tie = 'Tie'
+
+
 
 console.log(playerScoreElement);
 console.log(cpuScoreElement);
 
-/*function getPlayerSelection(){
-
-    playerSelection = prompt('Rock... Paper...Scissors').toUpperCase().trim();
-    
-    while (!(playerSelection == 'ROCK' || playerSelection == 'PAPER' || playerSelection == 'SCISSORS')){
-        console.log('OOPS! LOOKS LIKE SOMETHING WENT WRONG!\nDON\'T FORGET, THE THREE OPTIONS FOR ROCK, PAPER, SCISSORS ARE: \nROCK, PAPER, AND SCISSORS');
-        playerSelection = prompt('Rock... Paper...Scissors').toUpperCase().trim();
-    }
-    return playerSelection;
-}
-*/
 
 
 function getComputerSelection(){
@@ -39,102 +62,82 @@ function getComputerSelection(){
     return (computerSelection);
 }
 
-/*
-function singleRound(playerSelection, computerSelection) {
-    let tieMessage = "IT\'S A TIE";
-    let winMessage = "YOU WIN!";
-    let loseMessage = "YOU LOSE!";
-    let errorMessage = "WELL, THAT DIDN\'T WORK";
-
-    console.log('                ', playerSelection, ' vs ',computerSelection)
-    if (computerSelection == playerSelection){
-        return (tieMessage);
-    }
-    else if (computerSelection == 'ROCK' && playerSelection == 'PAPER'){
-        return (winMessage);
-    }
-    else if (computerSelection == 'PAPER' && playerSelection == 'SCISSORS'){
-        return (winMessage);
-    }
-    else if (computerSelection == 'SCISSORS' && playerSelection == 'ROCK'){
-        return (winMessage);
-    }
-    else if (computerSelection == 'ROCK' && playerSelection == 'SCISSORS'){
-        return (loseMessage);
-    }
-    else if (computerSelection == 'PAPER' && playerSelection == 'ROCK'){
-        return(loseMessage);
-    }
-    else if (computerSelection == 'SCISSORS' && playerSelection == 'PAPER'){
-        return(loseMessage);
-    }
-    else{
-        return(errorMessage);
-    }
-};
-*/
 function round(playerSelection){
     let computerSelection = getComputerSelection();
-    //console.log(computerSelection);
-    //console.log(playerSelection);
+
+    playerHandElement[0].innerText = playerSelection;
+    cpuHandElement[0].innerText = computerSelection;
 
     if (computerSelection == playerSelection){
         console.log('Tie');
+        results = tieMessage;
     }
     else if (computerSelection == 'ROCK' && playerSelection == 'PAPER'){
         console.log('Win');
         playerScore +=1;
         playerScoreElement[0].innerHtml = playerScore;
+        results = winMessage;
         
     }
     else if (computerSelection == 'PAPER' && playerSelection == 'SCISSORS'){
         console.log('Win');
         playerScore +=1;
         playerScoreElement[0].innerText = playerScore;
+        results = winMessage;
     }
     else if (computerSelection == 'SCISSORS' && playerSelection == 'ROCK'){
         console.log('Win');;
         playerScore +=1;
         playerScoreElement[0].innerText = playerScore;
+        results = winMessage;
     }
     else if (computerSelection == 'ROCK' && playerSelection == 'SCISSORS'){
         console.log('Lose');
         cpuScore +=1;
         cpuScoreElement[0].innerText = cpuScore;
+        results = loseMessage;
     }
     else if (computerSelection == 'PAPER' && playerSelection == 'ROCK'){
         console.log('Lose');
         cpuScore +=1;
         cpuScoreElement[0].innerText = cpuScore;
+        results = loseMessage;
     }
     else if (computerSelection == 'SCISSORS' && playerSelection == 'PAPER'){
         console.log('Lose');
         cpuScore +=1;
         cpuScoreElement[0].innerText = cpuScore;
+        results = loseMessage;
     }
     else{
         console.log('Error');
+        results = errorMessage;
     }
 
     if (playerScore == limit){
-        celebration();
-        resultsElement[0].innerText = 'You Won'
-        playerScoreElement[0].innerText = '0'
+        roundNumber = 0;
+        resultsElement[0].innerText = champMessage;
+        playerScoreElement[0].innerText = '0';
         cpuScoreElement[0].innerText = '0';
 
         ;
     } else if (cpuScore == limit){
-        consolation();
-        resultsElement[0].innerText = 'You Lost'
+        roundNumber = 0;
+        resultsElement[0].innerText = consolationMessage;
         playerScoreElement[0].innerText = '0'
         cpuScoreElement[0].innerText = '0';
     } else{
         resultsElement[0].innerText = ''
     }
-
+    
+    roundNumber ++;
+    roundNumberElement[0].innerText = roundNumber;
 
     playerScoreElement.innerHtml = playerScore;
     cpuScoreElement.innerHtml = cpuScore;
+    resultsElement[0].innerText = results
+
+
     console.log("Player: ", playerScore, "CUP:", cpuScore);
     //console.log('player element', playerScoreElement);
     //console.log('cpu element', cpuScoreElement);
